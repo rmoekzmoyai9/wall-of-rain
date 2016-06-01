6e27f38a49fde63885d46bd5bb0aef74edd72fcb
Warren Wright <Warren@glkdhkljhljhk.com>
2016-06-01 17:54:11 -0400

    array method is working

--- array_approach.php
+++ array_approach.php
@@ -29,14 +29,47 @@ for ($i = 0; $i < $maxLength; $i++) {
 // If the spot is a wall, move on
 // If it isn't a wall, check for walls to the left and right
 // If the check passes, tally up the water!
+$tally = 0;
 for ($i = 0; $i < $maxLength; $i++) {
 	for ($j = 0; $j < $maxHeight; $j++) {
 		if (!isset($wall[$i][$j])) {
-			puddleChecker($wall, $i, $j);
+			if (puddleChecker($wall, $i, $j, $maxLength)) {
+				$tally++;
+			}
 		}
 	}
 }
 
-function puddleChecker($theWall, $x, $y) {
-	//
+echo "Your total puddle space is " . $tally . "\n";
+
+function puddleChecker($theWall, $x, $y, $maxLength) {
+	if (puddleCheckerLeft($theWall, $x, $y, $maxLength)
+		and puddleCheckerRight($theWall, $x, $y, $maxLength)) {
+		return true;
+	}
+	return false;
+}
+
+function puddleCheckerLeft($theWall, $x, $y, $maxLength) {
+	if ($x == 0) {
+		return false;
+	}
+	for ($i = $x; $i >= 0; $i--) {
+		if (isset($theWall[$i][$y])) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function puddleCheckerRight($theWall, $x, $y, $maxLength) {
+	if ($x == $maxLength) {
+		return false;
+	}
+	for ($i = $x; $i < $maxLength; $i++) {
+		if (isset($theWall[$i][$y])) {
+			return true;
+		}
+	}
+	return false;
 }
